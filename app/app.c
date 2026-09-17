83c6b7d4b4cfc8eaa513ccbfff2ae85324b589a8
TRW <itgz2085@outlook.com>
2026-09-17 13:22:14 +0800

    添加超时记录

--- app/app.c
+++ app/app.c
@@ -2,6 +2,7 @@
 #include "app_cfg.h"
 //
 #include "bsp_log.h"
+#include "bsp_sys_status.h"
 #include "bsp_dwt.h"
 #include "lib_math.h"
 #include "bsp_freertos.h"
@@ -56,7 +57,10 @@ ITCM_RAM static __attribute__((noreturn)) void StartCmdTask(void *argument)
         AppCmdRun();
         dt = DWT_GetTimeUs() - start;
         if (dt > 1000 * CMD_FREQ_MS)
+        {
             BSPLOG(&g_app_log, LOG_LEVEL_ERROR, "CMD Task is being DELAY! dt = %llu(us)", dt);
+            BSP_ASSERT_TASK_TIMEOUT(); // 任务超时：系统状态计数 +1
+        }
     }
 }
 
@@ -74,7 +78,10 @@ ITCM_RAM static __attribute__((noreturn)) void StartGimbalTask(void *argument)
         AppGimbalRun();
         dt = DWT_GetTimeUs() - start;
         if (dt > 1000 * GIMBAL_FREQ_MS)
+        {
             BSPLOG(&g_app_log, LOG_LEVEL_ERROR, "GIMBAL Task is being DELAY! dt = %llu(us)", dt);
+            BSP_ASSERT_TASK_TIMEOUT(); // 任务超时：系统状态计数 +1
+        }
     }
 }
 
@@ -92,7 +99,10 @@ ITCM_RAM static __attribute__((noreturn)) void StartShootTask(void *argument)
         AppShootRun();
         dt = DWT_GetTimeUs() - start;
         if (dt > 1000 * SHOOT_FREQ_MS)
+        {
             BSPLOG(&g_app_log, LOG_LEVEL_ERROR, "SHOOT Task is being DELAY! dt = %llu(us)", dt);
+            BSP_ASSERT_TASK_TIMEOUT(); // 任务超时：系统状态计数 +1
+        }
     }
 }
 
